0751677a65cd4809088d7481baf66250f59a5a97
Michael Grundler <mcgrundler@gmail.com>
2016-11-11 14:05:33 -0500

    fix error in citation generation

--- SquamataBase/Workbench/templatetags/bibliography.py
+++ SquamataBase/Workbench/templatetags/bibliography.py
@@ -7,8 +7,7 @@ register = template.Library()
 
 @register.inclusion_tag('site/bibliography.html')
 def bibliography(ref):
-    authors = Contribution.objects.filter(ref=ref)
-    authors = Person.objects.filter(id__in=authors.values_list('id', flat=True)).order_by('id')
+    authors = Person.objects.filter(id__in=Contribution.objects.filter(ref=ref).values_list('person_id', flat=True))
     if ref.ref_type.term == 'journal_article':
         item = JournalArticle.objects.get(ref=ref)
     elif ref.ref_type.term == 'book_chapter':
@@ -17,7 +16,6 @@ def bibliography(ref):
         item = Book.objects.get(ref=ref)
     else:
         item = None
-    
     authorstr = format_html("<p>{}", " ".join([authors[0].first_name, authors[0].last_name]))
     for a in range(1, len(authors)):
         authorstr = format_html("{}<br>{}", mark_safe(authorstr), " ".join([authors[a].first_name, authors[a].last_name]))
