cdf5e7785069a6c7323178c3533f573819823702
Michael Grundler <mcgrundler@gmail.com>
2017-01-23 15:09:01 -0500

    minor change to settings

--- SquamataBase/settings_local.py
+++ SquamataBase/settings_local.py
@@ -1,4 +1,6 @@
 # May need to change depending on your installations
 SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
 
+# Check that coordinates fall within administrative boundary prior to saving
 VALIDATE_COORDINATES = True
+
